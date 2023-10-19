e3bd7e310013c6cd9c00fcab1e247e43d9a40adf
Erick <emgy1995@gmail.com>
2023-10-19 14:57:11 +0800

    added gif

--- tests/test_stream_page.py
+++ tests/test_stream_page.py
@@ -1,5 +1,5 @@
 from base_test import BaseTest
-
+import time
 
 from pages.stream_page import StreamPage
 
@@ -8,13 +8,18 @@ class TestStreamPage(BaseTest):
     
     def test_stream_screenshot(self):
         self.stream_page=StreamPage(self.driver)
+        sleep_time=3 #sleep time in case there's a content warning
         try:
             self.stream_page.click_content_warning_button()
         except:
+            #if no content warning was found then browser already waited 
+            sleep_time=0
             pass
         try:
             self.stream_page.get_chat_room_welcome()
         except:
             pass
+        
+        time.sleep(sleep_time)
         self.stream_page.take_screenshot('streamer_screenshot.png')
           
